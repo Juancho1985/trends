57349f98daeedff8afbbe89202ebf5cadf508d89
Juancho1985 <lgato8576@gmail.com>
2015-09-04 21:54:01 -0300

    There will be only two ambients in this project, production and development.

--- gulp-files/tasks/staging.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp'),
-config   = require('../config'),
-replace  = require('gulp-replace');
-
-gulp.task('staging', ['fonts', 'markup', 'images', 'minifyCss', 'uglifyJs'], function () {
-  return replace('staging', config);
-});
--- gulp-files/tasks/testing.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp'),
-config   = require('../config'),
-replace  = require('gulp-replace');
-
-gulp.task('testing', ['fonts', 'markup', 'images', 'minifyCss', 'uglifyJs'], function () {
-  return replace('testing', config);
-});
